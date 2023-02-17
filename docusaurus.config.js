5987826e813a7af3596899fe2c4c7a4201b22c1d
onyxgeek <onyxpeek@gmail.com>
2023-02-17 11:08:06 +0200

    feat:add new base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://vercel.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-websiteee/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
